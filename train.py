7dd54e84585a7238542520008952d9206f0e51f5
shengdie <shilishengdie@gmail.com>
2018-12-09 23:25:09 -0500

    4th try

--- train.py
+++ train.py
@@ -10,13 +10,13 @@ train_set, val_set, test_set, vocab = data()
 test_set = DataGen(test_set[0], test_set[1])
 val_set = DataGen(val_set[0], val_set[1])
 
-config = Config(['./configs/data_small.json', './configs/vocab_small.json', './configs/training_small.json',
+config = Config(['./configs/data.json', './configs/vocab.json', './configs/training.json',
                 './configs/model.json'])
-config.save('./results/small/')
+config.save('./results/large/')
 
-n_batches_epoch = ((len(train_set[1]) + config.batch_size - 1) //
-                        config.batch_size)
-
+#n_batches_epoch = ((len(train_set[1]) + config.batch_size - 1) //
+#                        config.batch_size)
+n_batches_epoch = 100
 lr_schedule = LRSchedule(lr_init=config.lr_init,
         start_decay=config.start_decay*n_batches_epoch,
         end_decay=config.end_decay*n_batches_epoch,
@@ -24,7 +24,7 @@ lr_schedule = LRSchedule(lr_init=config.lr_init,
         lr_warm=config.lr_warm,
         lr_min=config.lr_min)
 
-model = Img2SeqModel(config, './results/small/', vocab)
+model = Img2SeqModel(config, './results/large/', vocab)
 
 model.build_train(config)
 model.train(config, train_set, val_set, lr_schedule)
